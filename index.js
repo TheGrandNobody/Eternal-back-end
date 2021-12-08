0879242cf48931388817b1d9dd6f71976e130276
TheGrandNobody <thegrandnobody@protonmail.com>
2021-12-08 15:57:13 +0100

    Update

--- index.js
+++ index.js
@@ -13,19 +13,11 @@ server.use(bodyParser.json());
 server.use('/api', routes);
 server.use(handleError);
 
-// mongooseConnectionHandler
-//   .connect('mongodb+srv://tamjeed:Macbookpro1@cluster0.f92lg.mongodb.net/eternal?retryWrites=true&w=majority', {
-//     useNewUrlParser: true,
-//   })
-//   .catch((err) => console.log(err));
-
 mongooseConnectionHandler
-  .connect('mongodb://localhost:27017/eteranl', {
+  .connect('mongodb+srv://dbUser:testtesttest@cluster0.n6wy7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
   })
   .catch((err) => console.log(err));
-
-//localhost:27017
 
 mongodb: server.listen(port, (err) => {
   if (err) throw err;
